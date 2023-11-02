e26941d326248c41f497a31385c3fc2194f0aa75
Bryan <Bryan.wu.121@gmail.com>
2023-11-02 15:15:44 -0500

    can now connect to mysql

---
+++ backend/server.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+import mysql.connector, os
+from mysql.connector import errorcode
+from dotenv import load_dotenv
+
+load_dotenv()
+TABLES = {}
+TABLES['employees'] = (
+    "CREATE TABLE `employees` ("
+    "  `emp_no` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `birth_date` date NOT NULL,"
+    "  `first_name` varchar(14) NOT NULL,"
+    "  `last_name` varchar(16) NOT NULL,"
+    "  `gender` enum('M','F') NOT NULL,"
+    "  `hire_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`)"
+    ") ENGINE=InnoDB")
+
+
+try: 
+    con = mysql.connector.connect(user = "root", password = os.getenv('password'), host='127.0.0.1', port='3306', database='project_database')
+    cursor = con.cursor()
+    cursor.execute(TABLES['employees'])
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  con.close()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+@app.route("/")
+def home():
+    return "Hi, returning from flask"
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
