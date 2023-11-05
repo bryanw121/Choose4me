f117d7e0a1dc09d72a7530e1857bbd52e42c785e
Bryan <Bryan.wu.121@gmail.com>
2023-11-05 17:54:02 -0600

    updated backend

--- backend/server.py
+++ backend/server.py
@@ -6,17 +6,17 @@ from mysql.connector import errorcode
 from dotenv import load_dotenv
 
 load_dotenv()
-# TABLES = {}
-# TABLES['employees'] = (
-#     "CREATE TABLE `employees` ("
-#     "  `emp_no` int(11) NOT NULL AUTO_INCREMENT,"
-#     "  `birth_date` date NOT NULL,"
-#     "  `first_name` varchar(14) NOT NULL,"
-#     "  `last_name` varchar(16) NOT NULL,"
-#     "  `gender` enum('M','F') NOT NULL,"
-#     "  `hire_date` date NOT NULL,"
-#     "  PRIMARY KEY (`emp_no`)"
-#     ") ENGINE=InnoDB")
+TABLES = {}
+TABLES['employees'] = (
+    "CREATE TABLE `employees` ("
+    "  `emp_no` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `birth_date` date NOT NULL,"
+    "  `first_name` varchar(14) NOT NULL,"
+    "  `last_name` varchar(16) NOT NULL,"
+    "  `gender` enum('M','F') NOT NULL,"
+    "  `hire_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`)"
+    ") ENGINE=InnoDB")
 
 
 try: 
